4e051d6ab144c8ad946e1dab8501a73c021f3921
Lucas Gauer <lucas.gauer@reweb.com.br>
2016-03-10 12:25:32 -0300

    alteracao de estruturas de projeto e de deploy

---
+++ assets/frontend/js/page.js
@@ -0,0 +1,41 @@
+webpackJsonp([1,2],[
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	module.exports = __webpack_require__(2);
+
+
+/***/ },
+/* 1 */,
+/* 2 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+	
+	var _exclaimify = __webpack_require__(3);
+	
+	var _exclaimify2 = _interopRequireDefault(_exclaimify);
+	
+	console.log((0, _exclaimify2['default'])('page2.js loaded'));
+
+/***/ },
+/* 3 */
+/***/ function(module, exports) {
+
+	"use strict";
+	
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	
+	exports["default"] = function (string) {
+	  return string + "!";
+	};
+	
+	module.exports = exports["default"];
+
+/***/ }
+]);
+//# sourceMappingURL=page.js.map
