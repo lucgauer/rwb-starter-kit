20e77efad616510f33c81d6d7fd1a2612369be8d
Lucas Gauer <lucas.gauer@reweb.com.br>
2016-03-17 09:40:01 -0300

    adicionado bootstrap sass

---
+++ index.php
@@ -0,0 +1,11 @@
+<?
+    include 'views/index.html';
+?>
+gulpfile.js/extras/craft/README.md
+gulpfile.js/extras/craft/gulprev/GulpRevPlugin.php
+gulpfile.js/extras/craft/gulprev/twigextensions/GulpRevTwigExtension.php
+gulpfile.js/extras/rails/README.md
+gulpfile.js/extras/rails/app/helpers/gulp_asset_helper.rb
+gulpfile.js/extras/rails/config/deploy.rb.example
+gulpfile.js/extras/rails/config/initializers/gulp.rb
+gulpfile.js/tasks/iconFont/template.sass
